7b598ca2424d663a916cce3ddec040edb7c81201
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-06-26 06:01:51 -0300

    Add test code

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <xc.h>
 #include <pic16f877a.h>
 #include "lcd.h"
+#include "keypad.h"
 
 #define _XTAL_FREQ 8000000
 
@@ -19,8 +20,30 @@ void __interrupt() isr(void) {
 }
 
 void main(void) {
+
+    unsigned char temp = 0;
+
+
     initialize_lcd();
-    write_string("Insira a temperatura (°):");
+    keypad_init();
+    
+    write_string("Escolha a");
+    set_cursor(2, 1);
+    write_string("temperatura:");
+    __delay_ms(2000);
+
+    do
+    {
+        unsigned char key_pressed = scan_keypad();
+        clear_lcd();
+        set_cursor(1, 1);
+        __delay_ms(100);
+        write_char(key_pressed);
+        __delay_ms(100);
+    } while (temp == 0);
+    
+
+    while (1);
 
     return;
 }
