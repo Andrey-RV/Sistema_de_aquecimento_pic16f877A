540db1fbd96a9f0298e4c3affaf63c977402f906
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-06-26 19:57:10 -0300

    Add temperature selection in main

--- main.c
+++ main.c
@@ -9,19 +9,19 @@
 
 #include <xc.h>
 #include <pic16f877a.h>
+#include <stdio.h>
 #include "lcd.h"
 #include "keypad.h"
 
 #define _XTAL_FREQ 8000000
 
 
-void __interrupt() isr(void) {
-
-}
-
 void main(void) {
-
-    unsigned char temp = 0;
+    const unsigned char temperatures[5] = {10, 20, 30, 40 ,50};
+    unsigned char n = 0;
+    unsigned char choose_temp = 0;
+    unsigned char choose_temp_str[] = "10";
+    unsigned char key_pressed = 'n';
 
 
     initialize_lcd();
@@ -29,20 +29,46 @@ void main(void) {
     
     write_string("Escolha a");
     set_cursor(2, 1);
-    write_string("temperatura:");
-    __delay_ms(2000);
+    write_string("temperatura: 10 ");
 
-    do
-    {
-        unsigned char key_pressed = scan_keypad();
-        clear_lcd();
-        set_cursor(1, 1);
+    while (1) {
+        key_pressed = scan_keypad();
+
+        if (key_pressed == 'A') {
+            n++;
+            if (n > 4) {
+                n = 0;
+            }
+            choose_temp = temperatures[n];
+        }
+        else if (key_pressed == 'D') {
+            n--;
+            if (n < 0) {
+                n = 0;
+            }
+            choose_temp = temperatures[n];
+        }
+
+        sprintf(choose_temp_str, "%d", choose_temp);
+        set_cursor(2, 14);
+        write_string(choose_temp_str);
         __delay_ms(100);
-        write_char(key_pressed);
-        __delay_ms(100);
-    } while (temp == 0);
+
+        if (key_pressed == 'E') {
+            break;
+        }   
+        else if (key_pressed == 'C'){
+            choose_temp = 0;
+        }
+    }
     
-
+    clear_lcd();
+    set_cursor(1, 1);
+    write_string("Temperatura");
+    set_cursor(2, 1);
+    write_string("escolhida: ");
+    write_string(choose_temp_str);
+    
     while (1);
 
     return;
