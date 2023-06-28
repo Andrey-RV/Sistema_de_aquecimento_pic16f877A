14a90ed2b9fd8b5ad430f2c7543cf07235c0964d
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-06-28 10:37:49 -0300

    Move part of the code from main() to get_aimed_temperature()

--- main.c
+++ main.c
@@ -15,55 +15,52 @@
 #define _XTAL_FREQ 8000000
 
 
-void main(void) {
-    const unsigned char* temperatures[] = {"10", "20", "30", "40" ,"50"};
-    unsigned char n = 0;
-    unsigned char key_pressed = 'n';
+unsigned char* get_aimed_temperature();
 
 
+void main() {
     initialize_lcd();
     keypad_init();
-    
+
     write_string("Escolha a");
     set_cursor(2, 1);
     write_string("temperatura: 10 ");
+    unsigned char* chosen_temperature = get_aimed_temperature();
 
-    while (1) {
-        key_pressed = scan_keypad();
-
-        if (key_pressed == 'A') {
-            n++;
-            if (n > 4) {
-                n = 0;
-            }
-        }
-        else if (key_pressed == 'D') {
-            n--;
-            if (n < 0) {
-                n = 0;
-            }
-        }
-
-        set_cursor(2, 14);
-        write_string(temperatures[n]);
-        __delay_ms(100);
-
-        if (key_pressed == 'E') {
-            break;
-        }   
-        // else if (key_pressed == 'C'){
-            
-        // }
-    }
-    
     clear_lcd();
     set_cursor(1, 1);
     write_string("Temperatura");
     set_cursor(2, 1);
     write_string("escolhida: ");
-    write_string(temperatures[n]);
+    write_string(chosen_temperature);
     
     while (1);
 
     return;
+}
+
+unsigned char* get_aimed_temperature(){
+    const unsigned char* temperatures[] = {"10", "20", "30", "40" ,"50"};
+    char n = 0;
+    unsigned char key_pressed = 'n';
+
+    while (1) {
+        key_pressed = scan_keypad();
+
+        switch (key_pressed){
+            case 'A':
+                n = n == 4 ? 4 : n + 1;
+                set_cursor(2, 14);
+                write_string(temperatures[n]);
+                break;
+            case 'D':
+                n = n == 0 ? 0 : n - 1;
+                set_cursor(2, 14);
+                write_string(temperatures[n]);
+                break;
+            case 'E':
+                return temperatures[n];
+                break;
+        }
+    }
 }
