e9077e59fa189acbcf055f46f0b25ce9b4dd4173
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-07-11 01:22:16 -0300

    Change EN pulse duration from 1 us to 1 ms Change pins of EN and RS

--- lcd4.h
+++ lcd4.h
@@ -1,16 +1,16 @@
 #include <xc.h>
 #include <pic16f877a.h>
 
-#define EN PORTEbits.RE0
-#define RS PORTEbits.RE1
+#define EN PORTEbits.RE1
+#define RS PORTEbits.RE0
 #define data_bus PORTD
 #define _XTAL_FREQ 8000000
 
 void write_on_lcd_bus(char a);
 void send_lcd_command(char a);
+void initialize_lcd();
 void clear_lcd(void);
 void set_cursor(char a, char b);
-void initialize_lcd();
 void write_char(char a);
 void write_string(const char* a);
 
@@ -27,36 +27,6 @@ void send_lcd_command(char a)
     EN = 1;
     __delay_ms(1);
     EN = 0;
-}
-
-void clear_lcd(void)
-{
-    send_lcd_command(0x00);     // Clear display command part 1
-    __delay_ms(2);
-    send_lcd_command(0x01);      // Clear display command part 2
-    __delay_ms(2);
-    send_lcd_command(0x00);     // Return home command part 1
-    __delay_ms(2);
-    send_lcd_command(0x02);     // Return home command part 2
-}
-
-void set_cursor(char a, char b)
-{
-    char position, position_high, position_low;
-    if (a == 1){
-        position = 0x80 + b - 1;    // 0x80 command moves the cursor to the start of the first line
-        position_high = position & 0xF0;
-        position_low = position & 0x0F;
-        send_lcd_command(position_high >> 4);
-        send_lcd_command(position_low);      
-    }  
-    else if (a == 2){
-        position = 0xC0 + b - 1;    // 0xC0 command moves the cursor to the start of the second line
-        position_high = position & 0xF0;
-        position_low = position & 0x0F;
-        send_lcd_command(position_high >> 4);
-        send_lcd_command(position_low);
-    }
 }
 
 void initialize_lcd()
@@ -91,6 +61,46 @@ void initialize_lcd()
     __delay_ms(2);
 }
 
+void clear_lcd(void)
+{
+    send_lcd_command(0x00);     // Clear display command part 1
+    __delay_ms(2);
+    send_lcd_command(0x01);      // Clear display command part 2
+    __delay_ms(2);
+    send_lcd_command(0x00);     // Return home command part 1
+    __delay_ms(2);
+    send_lcd_command(0x02);     // Return home command part 2
+}
+
+void set_cursor(char a, char b)
+{
+    char position, position_high, position_low;
+    if (a == 1){
+        position = 0x80 + b - 1;    // 0x80 command moves the cursor to the start of the first line
+        position_high = position & 0xF0;
+        position_low = position & 0x0F;
+        send_lcd_command(position_high >> 4);
+        send_lcd_command(position_low);      
+    }  
+    else if (a == 2){
+        position = 0xC0 + b - 1;    // 0xC0 command moves the cursor to the start of the second line
+        position_high = position & 0xF0;
+        position_low = position & 0x0F;
+        send_lcd_command(position_high >> 4);
+        send_lcd_command(position_low);
+    }
+}
+
+void add_custom_char(const char* symbol, char address)
+{
+    int i;
+    send_lcd_command(0x40 | (address * 8));
+
+    for (i = 0; i < 8; i++) {
+        write_char(symbol[i]);
+    }
+}
+
 void write_char(char a)
 {
     char lower_nibble = a & 0x0F;
@@ -99,18 +109,18 @@ void write_char(char a)
     RS = 1;
     write_on_lcd_bus(higher_nibble >> 4);
     EN = 1;
-    __delay_us(1);
+    __delay_ms(1);
     EN = 0;
 
     write_on_lcd_bus(lower_nibble);
     EN = 1;
-    __delay_us(1);
+    __delay_ms(1);
     EN = 0;
 }
 
 void write_string(const char* a)
 {
-    int i;
+    char i;
     for (i = 0; a[i] != '\0'; i++)
         write_char(a[i]);
 }
