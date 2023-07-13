b50916ac52b38ec40577f6d9f8a0b2ae951bb8ec
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-07-13 13:34:09 -0300

    Remove functions definition to a corresponding c file

--- ad_converter.h
+++ ad_converter.h
@@ -1,29 +1,5 @@
-#include <xc.h>
-#include <pic16f877a.h>
-
 #define _XTAL_FREQ 8000000
+
 
 void ad_init(void);
 unsigned int read_ad(void);
-
-
-void ad_init(void){
-    ADCON0 = 0b11000001;
-    ADCON1 = 0b10000010;
-    TRISAbits.TRISA0 = 1;
-}
-
-unsigned int read_ad(void){
-    unsigned int adc_result = 0;
-    unsigned int voltage = 0;
-    unsigned int temperature = 0;
-
-    __delay_ms(100);
-    ADCON0bits.GO = 1;
-    while(ADCON0bits.GO);
-
-    adc_result = (ADRESH << 8) + ADRESL;
-    voltage = adc_result * 5;
-    temperature = voltage / 10;
-    return temperature;
-}
