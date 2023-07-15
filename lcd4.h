4fa16d2dd094d9af7811d52d6ed2dabccf3e83bf
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-07-15 00:40:12 -0300

    All functions expect unsigned chars (or const unsigned char)  instead of chars

--- lcd4.h
+++ lcd4.h
@@ -3,10 +3,10 @@
 #define data_bus PORTD
 #define _XTAL_FREQ 8000000
 
-void write_on_lcd_bus(char a);
-void send_lcd_command(char a);
-void initialize_lcd();
+void write_on_lcd_bus(unsigned char a);
+void send_lcd_command(unsigned char a);
+void initialize_lcd(void);
 void clear_lcd(void);
-void set_cursor(char a, char b);
-void write_char(char a);
-void write_string(const char* a);
+void set_cursor(unsigned char a, unsigned char b);
+void write_char(unsigned char a);
+void write_string(const unsigned char* a);
