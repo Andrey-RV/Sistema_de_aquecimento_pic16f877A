0d41405ed6783af1b4ddd81523a382706426ddc0
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-07-15 00:41:24 -0300

    All functions expect unsigned chars (or const unsigned char)  instead of chars

--- lcd4.c
+++ lcd4.c
@@ -3,12 +3,12 @@
 #include "lcd4.h"
 
 
-void write_on_lcd_bus(char a)
+void write_on_lcd_bus(unsigned char a)
 {
     data_bus = a << 4;
 }
 
-void send_lcd_command(char a)
+void send_lcd_command(unsigned char a)
 {
     RS = 0;
     write_on_lcd_bus(a);
@@ -17,7 +17,7 @@ void send_lcd_command(char a)
     EN = 0;
 }
 
-void initialize_lcd()
+void initialize_lcd(void)
 {
     TRISD = 0x00;
     TRISE = 0x00;
@@ -58,11 +58,12 @@ void clear_lcd(void)
     send_lcd_command(0x00);     // Return home command part 1
     __delay_ms(2);
     send_lcd_command(0x02);     // Return home command part 2
+    set_cursor(1, 1);
 }
 
-void set_cursor(char a, char b)
+void set_cursor(unsigned char a, unsigned char b)
 {
-    char position, position_high, position_low;
+    unsigned char position, position_high, position_low;
     if (a == 1){
         position = 0x80 + b - 1;    // 0x80 command moves the cursor to the start of the first line
         position_high = position & 0xF0;
@@ -79,10 +80,10 @@ void set_cursor(char a, char b)
     }
 }
 
-void write_char(char a)
+void write_char(unsigned char a)
 {
-    char lower_nibble = a & 0x0F;
-    char higher_nibble = a & 0xF0;
+    unsigned char lower_nibble = a & 0x0F;
+    unsigned char higher_nibble = a & 0xF0;
 
     RS = 1;
     write_on_lcd_bus(higher_nibble >> 4);
@@ -96,9 +97,9 @@ void write_char(char a)
     EN = 0;
 }
 
-void write_string(const char* a)
+void write_string(const unsigned char* a)
 {
-    char i;
+    unsigned char i;
     for (i = 0; a[i] != '\0'; i++)
         write_char(a[i]);
 }
