3e00f4e3108e508f17038b1d54322f80a71cd4d2
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-07-15 00:44:06 -0300

    Macros and prototypes moved to a separated header file; Add isr for timer1; char (and const char) are now unsigned char (or const unsigned char); Move pwm, keypad, ad converter and lcd initializations to initial_setup(); Add code related to showing temperature and timer to the LCD;

--- main.c
+++ main.c
@@ -1,59 +1,59 @@
-#pragma config FOSC = HS        
-#pragma config WDTE = OFF       
-#pragma config PWRTE = OFF      
-#pragma config BOREN = OFF      
-#pragma config LVP = OFF        
-#pragma config CPD = OFF        
-#pragma config WRT = OFF        
-#pragma config CP = OFF         
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <xc.h>
 #include <pic16f877a.h>
 #include "lcd4.h"
 #include "keypad.h"
-#include "pwm.h"
 #include "ad_converter.h"
+#include "pwm.h"
+#include "main.h"
 
-#define _XTAL_FREQ 8000000
 
-void get_aimed_temperature(void);
-void get_heating_time(void);
-void set_heating(char temperature);
-void set_timer(char time);
+long int timer_counter = 0;         // TMR1 is set to count up to 0.25s, so 4 interrupts are 1s
 
+
+void __interrupt() isr(void){
+    if (PIR1bits.TMR1IF){
+        PIR1bits.TMR1IF = 0;
+        timer_counter--;
+
+        if (timer_counter % 4 == 0){
+            show_temp_and_time();
+            if (timer_counter == 0){
+                PIE1bits.TMR1IE = 0;
+            }
+        }
+    }
+}
 
 void main(void) {
-    char str_temperature[3];
-    char seconds[3];
-    unsigned int int_temperature = 0;
+    unsigned char* chosen_temperature;
+    unsigned char* chosen_time;
 
-    initialize_lcd();
-    keypad_init();
-    ad_init();
-    pwm_init();
-    change_pwm_duty_cycle(100);
-    
-    get_aimed_temperature();
-    __delay_ms(2000);
-    get_heating_time();
+    initial_setup();
+    chosen_temperature = get_aimed_temperature();
+    __delay_ms(1500);
+    chosen_time = get_heating_time();
+    __delay_ms(1500);
+    set_heating(chosen_temperature);
+    set_timer(chosen_time);
 
-    while (1){
-        __delay_ms(1000);
-        int_temperature = read_ad();
-        sprintf(str_temperature, "%d", int_temperature);
-        clear_lcd();
-        write_string(str_temperature);
-        write_string("C");
-    }
+    while (1);
 
     return;
 }
 
-void get_aimed_temperature(void){
-    const char* temperatures[] = {"10", "20", "30", "40" ,"50"};
-    char n = 0;
-    char key_pressed = 'n';
+void initial_setup(void){
+    initialize_lcd();
+    keypad_init();
+    ad_init();
+    pwm_init();
+}
+
+unsigned char* get_aimed_temperature(void){
+    const unsigned char* temperatures[] = {"10", "20", "30", "40" ,"50"};
+    unsigned char n = 0;
+    unsigned char key_pressed = 'n';
 
     write_string("Escolha a");
     set_cursor(2, 1);
@@ -80,21 +80,24 @@ void get_aimed_temperature(void){
             break;
         }
     }
+    display_aimed_temperature(temperatures[n]);
+    return temperatures[n];
+}
+
+void display_aimed_temperature(const unsigned char* temperature){
     clear_lcd();
-    set_cursor(1, 1);
     write_string("Temperatura");
     set_cursor(2, 1);
     write_string("escolhida: ");
-    write_string(temperatures[n]);
+    write_string(temperature);
 }
 
-void get_heating_time(void){
-    const char* time_intervals[] = {"10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60"};
-    char n = 4;
-    char key_pressed = 'n';
+unsigned char* get_heating_time(void){
+    const unsigned char* time_intervals[] = {"10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60"};
+    unsigned char n = 4;
+    unsigned char key_pressed = 'n';
 
     clear_lcd();
-    set_cursor(1, 1);
     write_string("Tempo de");
     set_cursor(2, 1);
     write_string("aquecimento: ");
@@ -122,11 +125,84 @@ void get_heating_time(void){
             break;
         }
     }
-    
+    display_heating_time(time_intervals[n]);
+    return time_intervals[n];
+}
+
+void display_heating_time(const unsigned char* time){
     clear_lcd();
-    set_cursor(1, 1);
     write_string("Tempo");
     set_cursor(2, 1);
     write_string("escolhido: ");
-    write_string(time_intervals[n]);
+    write_string(time);
+}
+
+void set_heating(const unsigned char* temperature){
+    if (temperature == "10"){
+        change_pwm_duty_cycle(10);
+    }
+    else if (temperature == "20"){
+        change_pwm_duty_cycle(20);
+    }
+    else if (temperature == "30"){
+        change_pwm_duty_cycle(30);
+    }
+    else if (temperature == "40"){
+        change_pwm_duty_cycle(40);
+    }
+    else if (temperature == "50"){
+        change_pwm_duty_cycle(50);
+    }
+    else if (temperature == "60"){
+        change_pwm_duty_cycle(60);
+    }
+    else if (temperature == "70"){
+        change_pwm_duty_cycle(70);
+    }
+    else if (temperature == "80"){
+        change_pwm_duty_cycle(80);
+    }
+    else if (temperature == "90"){
+        change_pwm_duty_cycle(90);
+    }
+    else if (temperature == "100"){
+        change_pwm_duty_cycle(100);
+    }
+    else {
+        change_pwm_duty_cycle(0);
+    }
+}
+
+void set_timer(const unsigned char* time){
+    timer_counter = 4 * strtol(time, NULL, 10);
+
+    T1CON = 0b00110001;                 // 1:8 prescaler, 16-bit timer, internal clock, no sync
+    TMR1 = 62500;                       // 0.25s
+
+    PIR1bits.TMR1IF = 0;
+    PIE1bits.TMR1IE = 1;
+    INTCONbits.PEIE = 1;
+    INTCONbits.GIE = 1;
+}
+
+void show_temp_and_time(void){
+    unsigned char str_current_temp[3];
+    unsigned char str_current_time[3];
+    unsigned int int_current_temp = read_ad();
+
+    sprintf(str_current_temp, "%d", int_current_temp);
+    sprintf(str_current_time, "%ld", timer_counter / 4);
+    clear_lcd();
+    write_string(str_current_temp);
+    write_string("C");
+    set_cursor(2, 1);
+    if (timer_counter == 0){
+        clear_lcd();
+        write_string("Aquecimento");
+        set_cursor(2, 1);
+        write_string("terminado!");
+        return;
+    }
+    write_string(str_current_time);
+    write_string("s");
 }
