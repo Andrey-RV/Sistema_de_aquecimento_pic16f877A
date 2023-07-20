9036557fa5f35a3d59ebf416dc6f979316c8ee85
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-07-20 00:39:00 -0300

    Change expected analog input to come from RA3 instead of RA0

--- ad_converter.c
+++ ad_converter.c
@@ -6,7 +6,7 @@
 void ad_init(void){
     ADCON0 = 0b11011001;
     ADCON1 = 0b10000010;
-    TRISAbits.TRISA0 = 1;
+    TRISAbits.TRISA3 = 1;
 }
 
 unsigned int read_ad(void){
@@ -18,8 +18,8 @@ unsigned int read_ad(void){
     ADCON0bits.GO = 1;
     while(ADCON0bits.GO);
 
-    adc_result = (ADRESH << 8) + ADRESL;
-    voltage = adc_result * 5;
-    temperature = voltage / 10;
+    adc_result = (ADRESH << 8) + ADRESL;      // Left justified
+    voltage = adc_result * 5;                 // 5V reference  
+    temperature = voltage / 10;               // 10mV per degree Celsius
     return temperature;
 }
