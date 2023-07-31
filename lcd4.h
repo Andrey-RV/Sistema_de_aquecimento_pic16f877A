1efbf32a593682524738667f10c316be611dbb30
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2023-07-31 05:16:03 -0300

    Add control_bus, data_bus_direction and control_bus_direction macros

--- lcd4.h
+++ lcd4.h
@@ -1,6 +1,9 @@
 #define EN PORTEbits.RE1
 #define RS PORTEbits.RE0
 #define data_bus PORTD
+#define control_bus PORTE
+#define data_bus_direction TRISD
+#define control_bus_direction TRISE
 #define _XTAL_FREQ 8000000
 
 void write_on_lcd_bus(unsigned char a);
